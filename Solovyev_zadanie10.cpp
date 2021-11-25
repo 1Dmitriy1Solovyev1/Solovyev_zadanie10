ebbeb8ed1a87e75057ccaa81cd6735dbc42c0153
serma <serma@DESKTOP-AD83IO3>
2021-11-25 13:46:15 +0300

    Добавьте файлы проекта.

---
+++ Solovyev_zadanie10.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int N;
+
+    cin >> N;
+
+    int* arr = new int[N];
+
+    int sum = 0;
+
+    for (int i = 1; i <= N; ++i) {
+
+        cin >> arr[i - 1];
+
+        sum += arr[i - 1];
+
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < N; ++i) {
+
+        if (arr[i] < (sum / N))
+
+            ++count;
+
+    }
+
+    cout << count;
+
+}
